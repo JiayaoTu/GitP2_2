8984b6f0e39710eecefafa70da9255df99ea9848
Kingtanna <15109256726@163.com>
2018-03-30 21:14:16 +0800

    commit_Section1_PracticeB

--- src/main/java/cn/school/thoughtworks/section1/PracticeB.java
+++ src/main/java/cn/school/thoughtworks/section1/PracticeB.java
@@ -6,7 +6,23 @@ public class PracticeB {
     List<String> collectSameElements(List<String> collection1, List<List<String>> collection2) {
         //实现练习要求，并改写该行代码。
 
-        return null;
+        List<String> result = new ArrayList<String>();
+        Set<String> set1 = new LinkedHashSet<>();
+        for (int i = 0; i < collection2.size(); i++) {
+            List<String> temp = collection2.get(i);
+            for (String s : collection1) {
+                if (temp.contains(s)) {
+                    set1.add(s);
+                }
+            }
+
+        }
+        for (String ss : set1)
+        {
+            result.add(ss);
+        }
+        return result;
+        
     }
 }
 
